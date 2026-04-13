7ec53958f9f88c6c5412867c364fdbf7f510fc89
Avinash <73986562+N-6051@users.noreply.github.com>
2026-04-14 02:08:26 +0530

    Create manifest.js for Calculator app
    Added a manifest file for the Calculator app with metadata.

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "lang": "en-us",
+  "name": "Calculator",
+  "short_name": "Calculator",
+  "description": "An arbitrary precision Calculator",
+  "start_url": "./",
+  "background_color": "#020a15",
+  "theme_color": "#020a15",
+  "orientation": "portrait-primary",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "./res/icon-512.png",
+      "sizes": "512x512"
+    },
+    {
+      "src": "./res/icon-128.png",
+      "sizes": "128x128"
+    }
+  ]
+  
+}
